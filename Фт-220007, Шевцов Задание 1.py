2e04537082440036c088295bbf32534182d03324
mnxactep <MNXACTEP2@gmail.com>
2023-09-29 00:44:57 +0500

    Добавил в программу вывод частного введенных чисел с точность до сотых 0,00

--- Фт-220007, Шевцов Задание 1.py
+++ Фт-220007, Шевцов Задание 1.py
@@ -6,9 +6,11 @@ difference = a - b
 product = a * b
 average = (a + b) / 2
 absolute_difference = max(abs(a), abs(b)) - min(abs(a), abs(b))
+quotient = round(a / b, 2)
 
 print("Сумма чисел:", sum)
 print("Разность чисел:", difference)
 print("Произведение чисел:", product)
 print("Среднее арифметическое чисел:", average)
 print("Разность максимального и минимального по модулю:", absolute_difference)
+print("Частное чисел:", quotient)
