50a96414fb3848d3b8bfe58d9b13a96024cc6a8b
mnxactep <146367248+mnxactep@users.noreply.github.com>
2023-09-29 00:08:27 +0500

    Загрузил код от задания 1.1 лабораторной работы 1

---
+++ Фт-220007, Шевцов Задание 1.py
@@ -0,0 +1,14 @@
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+
+sum = a + b
+difference = a - b
+product = a * b
+average = (a + b) / 2
+absolute_difference = max(abs(a), abs(b)) - min(abs(a), abs(b))
+
+print("Сумма чисел:", sum)
+print("Разность чисел:", difference)
+print("Произведение чисел:", product)
+print("Среднее арифметическое чисел:", average)
+print("Разность максимального и минимального по модулю:", absolute_difference)
